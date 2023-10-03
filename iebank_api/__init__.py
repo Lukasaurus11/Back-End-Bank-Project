414da897d98ecc330175f75a04b4d3b319f422f5
Lukasaurus11 <lucacuneob@gmail.com>
2023-10-03 15:26:56 +0200

    Update

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -3,30 +3,41 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from applicationinsights.flask.ext import AppInsights
 
 app = Flask(__name__)
 
 load_dotenv()
 
 # Select environment based on the ENV environment variable
-if os.getenv('ENV') == 'local':
+if os.getenv("ENV") == "local":
     print("Running in local mode")
-    app.config.from_object('config.LocalConfig')
-elif os.getenv('ENV') == 'dev':
+    app.config.from_object("config.LocalConfig")
+elif os.getenv("ENV") == "dev":
     print("Running in development mode")
-    app.config.from_object('config.DevelopmentConfig')
-elif os.getenv('ENV') == 'ghci':
+    app.config.from_object("config.DevelopmentConfig")
+elif os.getenv("ENV") == "ghci":
     print("Running in github mode")
-    app.config.from_object('config.GithubCIConfig')
+    app.config.from_object("config.GithubCIConfig")
 else:
     print("Running in production mode")
-    app.config.from_object('config.ProductionConfig')
-
+    app.config.from_object("config.ProductionConfig")
 
 db = SQLAlchemy(app)
 
 from iebank_api.models import Account
-db.create_all()
+
+with app.app_context():
+    db.create_all()
 CORS(app)
 
 from iebank_api import routes
+
+# Initialize Application Insights and force flushing application insights handler after each request
+if os.getenv("ENV") == "dev":
+    appinsights = AppInsights(app)
+
+    @app.after_request
+    def after_request(response):
+        appinsights.flush()
+        return response
--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -3,55 +3,61 @@ from iebank_api import db, app
 from iebank_api.models import Account
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/skull', methods=['GET'])
+
+@app.route("/skull", methods=["GET"])
 def skull():
-    return 'Hi! This is the BACKEND SKULL! 💀'
+    return "Hi! This is the BACKEND SKULL! 💀"
 
 
-@app.route('/accounts', methods=['POST'])
+@app.route("/accounts", methods=["POST"])
 def create_account():
-    name = request.json['name']
-    currency = request.json['currency']
+    name = request.json["name"]
+    currency = request.json["currency"]
     account = Account(name, currency)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
 
-@app.route('/accounts', methods=['GET'])
+
+@app.route("/accounts", methods=["GET"])
 def get_accounts():
     accounts = Account.query.all()
-    return {'accounts': [format_account(account) for account in accounts]}
+    return {"accounts": [format_account(account) for account in accounts]}
 
-@app.route('/accounts/<int:id>', methods=['GET'])
+
+@app.route("/accounts/<int:id>", methods=["GET"])
 def get_account(id):
     account = Account.query.get(id)
     return format_account(account)
 
-@app.route('/accounts/<int:id>', methods=['PUT'])
+
+@app.route("/accounts/<int:id>", methods=["PUT"])
 def update_account(id):
     account = Account.query.get(id)
-    account.name = request.json['name']
+    account.name = request.json["name"]
     db.session.commit()
     return format_account(account)
 
-@app.route('/accounts/<int:id>', methods=['DELETE'])
+
+@app.route("/accounts/<int:id>", methods=["DELETE"])
 def delete_account(id):
     account = Account.query.get(id)
     db.session.delete(account)
     db.session.commit()
     return format_account(account)
 
+
 def format_account(account):
     return {
-        'id': account.id,
-        'name': account.name,
-        'account_number': account.account_number,
-        'balance': account.balance,
-        'currency': account.currency,
-        'status': account.status,
-        'created_at': account.created_at
+        "id": account.id,
+        "name": account.name,
+        "account_number": account.account_number,
+        "balance": account.balance,
+        "currency": account.currency,
+        "status": account.status,
+        "created_at": account.created_at,
     }
