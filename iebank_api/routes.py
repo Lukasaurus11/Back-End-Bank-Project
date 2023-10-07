d466a435ac4c6f3724f7cf82071269829d4b8826
Lukasaurus11 <lucacuneob@gmail.com>
2023-10-07 15:57:51 +0200

    This will not work

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -17,7 +17,8 @@ def skull():
 def create_account():
     name = request.json["name"]
     currency = request.json["currency"]
-    account = Account(name, currency)
+    country = request.json["country"]
+    account = Account(name, currency, country)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
@@ -60,4 +61,5 @@ def format_account(account):
         "currency": account.currency,
         "status": account.status,
         "created_at": account.created_at,
+        "country": account.country,
     }
