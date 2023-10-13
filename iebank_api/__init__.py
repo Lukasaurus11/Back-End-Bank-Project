6584181d114715eddf421f133feed5179d52379f
Lukasaurus11 <lucacuneob@gmail.com>
2023-10-13 18:12:59 +0200

    Updated __init__.py (for the AppInsights)

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -34,7 +34,7 @@ CORS(app)
 from iebank_api import routes
 
 # Initialize Application Insights and force flushing application insights handler after each request
-if os.getenv("ENV") == "dev":
+if os.getenv("ENV") == "dev" or os.getenv("ENV") == "uat":
     appinsights = AppInsights(app)
 
     @app.after_request
