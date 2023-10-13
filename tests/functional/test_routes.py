9c2ab5f32d65c70ba47310fd8d917368ffb45f2a
Lukasaurus11 <lucacuneob@gmail.com>
2023-10-13 15:46:35 +0200

    Test

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -35,18 +35,6 @@ def test_create_account(testing_client):
     assert response.status_code == 200
 
 
-def test_create_account_wrong_json(testing_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/accounts' page is posted to (POST)
-    THEN check the response is valid
-    """
-    response = testing_client.post(
-        "/accounts", json={"name": "John Doe", "country": "Spain"}
-    )
-    assert response.status_code == 400
-
-
 def test_delete_account(testing_client):
     """
     GIVEN a Flask application
@@ -60,4 +48,20 @@ def test_delete_account(testing_client):
     )
 
     response = testing_client.delete("/accounts/1")
+    assert response.status_code == 200
+
+
+def test_get_account_by_id(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is requested (GET)
+    THEN check the response is valid
+    """
+
+    # Create an account first
+    response = testing_client.post(
+        "/accounts", json={"name": "John Doe", "country": "Spain", "currency": "€"}
+    )
+
+    response = testing_client.get("/accounts/1")
     assert response.status_code == 200
